7bdedb8efa7e299736ebd1eab5bb08468a03e616
rushtamizha <159513587+rushtamizha@users.noreply.github.com>
2026-03-13 18:56:11 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,19 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sonewaydroptaxi.in/",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://sonewaydroptaxi.in/",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://sonewaydroptaxi.in/",
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+  ];
+}
