b9152ae6581bd048ff2fdc8ae5b173afcda8962e
rushtamizha <159513587+rushtamizha@users.noreply.github.com>
2026-04-26 08:52:18 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -41,7 +41,8 @@ const HeroSection = () => {
               🚕 Intercity Specialists
             </span>
             <h1 className="text-5xl md:text-7xl font-black leading-[0.95] mb-6 tracking-tighter uppercase">
-              S TAXI Tamil Nadu’s Trusted <br />
+              <span className="text-[#489cc2]">S </span>
+              TAXI Tamil Nadu’s Trusted <br />
               <span className="text-[#489cc2]">One Way</span> <br />
               Drop Taxi Service
             </h1>
